2a6f47d55126be398c956851d21d773c3b745e45
Aidan Matrician <aidanmatrician@gmail.com>
2024-06-18 17:28:09 -0400

    added logs

--- ph_reading.py
+++ ph_reading.py
@@ -1,17 +1,21 @@
 import serial
-import time
+import datetime
 
 # Initialize serial communication
 PORTNAME = '/dev/ttyUSB0' # Adjust the port name as needed
-ser = serial.Serial(PORTNAME, 9600, timeout=1) 
-
+ser = serial.Serial(PORTNAME, 9600, timeout=1)
+print(ser)
 def read_ph():
+    print('started read_ph() at ' + datetime.datetime.now())
+
     # Send a command to the EZO module to take a reading
     ser.write(b'R\r')
+    print('wrote command to EZO module')
     time.sleep(1.5)  # Wait for the reading to complete
     
     # Read the data from the EZO module
     data = ser.readline().decode('utf-8').strip()
+    print('data returned from reading: ' + data)
     
     return data
 
